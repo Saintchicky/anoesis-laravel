f3d9eafbf2ed86ac02cea019b74343aea4756449
Leo <poussin6494@hotmail.com>
2018-11-08 20:40:02 +0100

    menu 1

---
+++ public/js/script.js
@@ -0,0 +1,13 @@
+
+    // Works everywhere
+    $(document).ready(function () {
+    
+    // Hide/show animation hamburger function
+    $('.navbar-toggler').on('click', function () {
+    
+    // Take this line to first hamburger animations
+    $('.animated-icon1').toggleClass('open');
+    
+    });
+    
+    });
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+                <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>Anoesis</title>
 
@@ -10,13 +12,33 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
         <!-- Styles -->
         <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
+        <link href="{{asset('css/style.css')}}" rel="stylesheet" type="text/css">
 
         <!-- Scripts -->
         <script src="{{asset('js/app.js')}}" type="text/javascript"></script>
+        {{-- <script src="{{asset('js/jquery-3.3.1.min.js')}}" type="text/javascript"></script> --}}
+       
+        
+        <script src="{{asset('js/script.js')}}" type="text/javascript"></script>
+
        
     </head>
     <body>
-    
- 
+    <header>
+        
+            <nav class="navbar navbar-dark bg-dark">
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                <div class="animated-icon1"><span></span><span></span><span></span></div>
+              </button>
+            </nav>
+            <div class="pos-f-t">
+                <div class="collapse" id="navbarToggleExternalContent">
+                  <div class="bg-dark p-4">
+                    <h5 class="text-white h4">Collapsed content</h5>
+                    <span class="text-muted">Toggleable via the navbar brand.</span>
+                  </div>
+                </div>
+          </div>
+    </header>
     </body>
 </html>
