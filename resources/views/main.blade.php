9c2ded75b7c2a989907680b6376b261d53c7de9a
Leo <poussin6494@hotmail.com>
2018-11-25 18:19:12 +0100

    loading

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,5 +1,9 @@
 @include('include.head')
 <body data-spy="scroll" data-target="#navbar-example2" data-offset="10">
+     <div id="overlay_intro">
+        <div id="progstat"></div>
+        <div id="progress"></div>
+    </div>
     @include('sections.header')
         @yield('content')
     @include('include.footer')
